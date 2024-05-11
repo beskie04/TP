10f38d46f51f40860e886b953ca8b068ef8c39e8
rawane0310 <118125151+rawane0310@users.noreply.github.com>
2024-05-11 11:37:52 +0100

    Update SeanceSuivi.java

--- SeanceSuivi.java
+++ SeanceSuivi.java
@@ -1,3 +1,6 @@
 public class SeanceSuivi extends RendezVous{
-
+ private int NumDossier;
+    private boolean type; // vraie si en ligne
+    private int note;
+    private Objectif [][] objectifsAtteints;
 }
